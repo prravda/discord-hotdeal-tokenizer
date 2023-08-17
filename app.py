92c4bb4aeb9a82cb807935c2c935d192926dcb5c
prravda <sonofhuman20@hufs.ac.kr>
2023-08-18 01:21:25 +0900

    impl: - created .proto file for defining gRPC and protobuf

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from kiwipiepy import Kiwi
+import tokenizer_definition_pb2
+
 kiwi = Kiwi()
 
-hotdeal_title = "[지마켓] 가시제거연구소 노르웨이 순살고등어 오렌지라벨 800g+800g (20,630원) (무료)"
-result = kiwi.tokenize(hotdeal_title)
+result = tokenizer_definition_pb2.TokenizingResult()
 
-for r in result:
-    print(r.form)
+hotdeal_title = "[지마켓] 가시제거연구소 노르웨이 순살고등어 오렌지라벨 800g+800g (20,630원) (무료)"
+tokenizer_result = kiwi.tokenize(hotdeal_title)
+
+extracted_tokens = []
+for t in tokenizer_result:
+    extracted_tokens.append(t.form)
+
+result.tokens.extend(extracted_tokens)
+
+print(result)
