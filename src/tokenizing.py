5277c86a62c28e066b938a09d476f77225ee6559
prravda <sonofhuman20@hufs.ac.kr>
2023-08-19 18:47:28 +0900

    impl: - try to wrap up functions with asyncio

--- src/tokenizing.py
+++ src/tokenizing.py
@@ -1,4 +1,5 @@
 import grpc
+import asyncio
 
 from concurrent import futures
 from proto_built_result import tokenizer_definition_pb2_grpc as tokenizer_grpc
@@ -9,8 +10,11 @@ from kiwipiepy import Kiwi
 kiwi_instance = Kiwi()
 
 
-def tokenize(sentence: tokenizer.TokenizingRequest) -> tokenizer.TokenizingResult:
-    parsed_tokens = kiwi_instance.tokenize(sentence.title)
+async def tokenize(sentence: tokenizer.TokenizingRequest) -> tokenizer.TokenizingResult:
+    loop = asyncio.get_event_loop()
+
+    with futures.ThreadPoolExecutor() as pool:
+        parsed_tokens = await loop.run_in_executor(pool, kiwi_instance.tokenize, sentence.title)
 
     protobuf_result = tokenizer.TokenizingResult()
 
@@ -23,21 +27,23 @@ def tokenize(sentence: tokenizer.TokenizingRequest) -> tokenizer.TokenizingResul
 
 
 class TokenizerServicer(tokenizer_grpc.TokenizerServicer):
-    def Tokenize(self, request: tokenizer.TokenizingRequest, unused_context) -> tokenizer.TokenizingResult:
-        return tokenize(request)
+    async def Tokenize(self, request: tokenizer.TokenizingRequest, unused_context) -> tokenizer.TokenizingResult:
+        return await tokenize(request)
 
 
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    tokenizer_grpc.add_TokenizerServicer_to_server(TokenizerServicer(), server)
+async def serve():
+    server = grpc.aio.server(futures.ThreadPoolExecutor())
+    tokenizer_grpc.add_TokenizerServicer_to_server(
+        TokenizerServicer(), server
+    )
 
     port_number = 50051
     server.add_insecure_port(f'[::]:{port_number}')
 
-    server.start()
+    await server.start()
 
     print(f'gRPC server listen on {port_number}!')
 
-    server.wait_for_termination()
+    await server.wait_for_termination()
 
-serve()
+asyncio.get_event_loop().run_until_complete(serve())
